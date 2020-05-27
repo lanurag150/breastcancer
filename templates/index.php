ab65b04349f39c7be47f98b67034b32ce1e12bad
lanurag150 <60335727+lanurag150@users.noreply.github.com>
2020-05-27 13:38:00 +0530

    Update index.php

--- templates/index.php
+++ templates/index.php
@@ -67,7 +67,7 @@
         <div class="row no-gutters slider-text align-items-center justify-content-center" data-scrollax-parent="true">
           <div class="col-md-8 ftco-animate text-center">
             <p class="breadcrumbs"><span class="mr-2"><a href="http://premediccare.rf.gd/index.php">Home</a></span></p>
-            <h3 class="mb-3 bread">CardioVascular-Department</h3>
+            <h3 class="mb-3 bread">BreastCancer-Department</h3>
           </div>
         </div>
       </div>
